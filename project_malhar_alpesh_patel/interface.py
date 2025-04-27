160b917bea33e6135be45c991558212703d1ac38
Mal-Pat <malhar30000@gmail.com>
2025-04-27 18:43:51 +0530

    interface finalized

--- project_malhar_alpesh_patel/interface.py
+++ project_malhar_alpesh_patel/interface.py
@@ -10,4 +10,10 @@ from dataset import AircraftDataset as TheDataset
 
 from dataset import get_dataloader as the_dataloader
 
+# This is used to transfer the data but isn't present in the instructions
 from dataset import data_transforms as the_datatransform
+
+# These were present in the original Final Project instructions, but not the recent one
+# So I still added them in just in case
+from config import NUM_EPOCHS as total_epochs
+from config import BATCH_SIZE as the_batch_size
